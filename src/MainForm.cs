b7507ca53b82b89aa0b36e4ab289a920bec61578
kochtaziz <kochtaziz8@gmail.com>
2026-08-08 16:45:20 +0200

    Restrict stock strategy to admins

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -119,7 +119,6 @@ namespace OlyDrugstorePOS
                 BuildSalesTab();
                 BuildCashTab();
                 BuildSalesHistoryTab();
-                BuildStockStrategyTab();
                 BuildReportsTab();
             }
             BuildSettingsTab();
